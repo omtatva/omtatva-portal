628763e7a3fbe719f652ad48dc2235c5968c7118
Omtatva Digitals <admin@omtatvadigitals.com>
2026-07-17 12:25:42 +0530

    updated

---
+++ app/workspace/Frameo/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+export default function WorkspacePage() {
+  return (
+    <iframe
+      src="https://frameo.ai/"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  );
+}
---
+++ app/workspace/higgsfield/page.js
@@ -0,0 +1,29 @@
+"use client";
+
+export default function WorkspacePage() {
+  return (
+    <div
+      style={{
+        padding: "20px",
+        height: "100vh",
+      }}
+    >
+      <h1>🎬 Higgsfield Workspace</h1>
+
+      <iframe
+        src="https://higgsfield.ai/"
+        width="100%"
+        height="850"
+        style={{
+          border: "none",
+          borderRadius: "12px",
+         }}
+        
+      />
+    </div>
+
+    
+
+    
+  );
+}
